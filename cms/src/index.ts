557769b31af54f82b0d6789a03c2d4fcee8f7bdd
tmsgroupeu <chrysostomoss@tmsgroupeu.com>
2025-10-28 12:33:12 +0200

    Update index.ts

--- cms/src/index.ts
+++ cms/src/index.ts
@@ -1,13 +1,4 @@
-// import type { Core } from '@strapi/strapi';
-
-export default {
-  /**
-   * An asynchronous register function that runs before
-   * your application is initialized.
-   *
-   * This gives you an opportunity to extend code.
-   */
-  register(/* { strapi }: { strapi: Core.Strapi } */) {},
+// cms/src/index.ts
 export default {
   register() {},
 
@@ -35,13 +26,4 @@ export default {
 
     strapi.log.info(`[bootstrap] Admin created: ${email}`);
   },
-};
-  /**
-   * An asynchronous bootstrap function that runs before
-   * your application gets started.
-   *
-   * This gives you an opportunity to set up your data model,
-   * run jobs, or perform some special logic.
-   */
-  bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
 };
