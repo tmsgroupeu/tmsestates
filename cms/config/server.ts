5eb49f0b6074f59848a761dd406f6297ab0b2e10
tmsgroupeu <chrysostomoss@tmsgroupeu.com>
2025-10-28 14:57:56 +0200

    Update server.ts

--- cms/config/server.ts
+++ cms/config/server.ts
@@ -1,13 +1,8 @@
-// cms/config/server.ts
+// /config/server.ts
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-
-  // public HTTPS URL so Strapi generates correct links
-  url: env('PUBLIC_URL'), // e.g. https://tmsestates.onrender.com
-
-  // trust Render’s proxy so req.secure is true and cookies can be “secure”
-  proxy: true,
-
+  url: env('PUBLIC_URL', 'https://tmsestates.onrender.com'),
   app: { keys: env.array('APP_KEYS') },
+  proxy: { koa: true }, // trust X-Forwarded-* from Render
 });
