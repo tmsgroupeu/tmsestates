79489e5c2e5b4a91073c49aaecd4fddb06a243bc
tmsgroupeu <chrysostomoss@tmsgroupeu.com>
2025-10-28 17:59:19 +0200

    Create .eslintrc.cjs

---
+++ web/.eslintrc.cjs
@@ -0,0 +1,10 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ["next/core-web-vitals"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+  },
+};
