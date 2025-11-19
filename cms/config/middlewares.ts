20d93d2336a53e591dedfe986f3521daf16171c9
tmsgroupeu <chrysostomoss@tmsgroupeu.com>
2025-11-19 21:02:12 +0200

    Update middlewares.ts

--- cms/config/middlewares.ts
+++ cms/config/middlewares.ts
@@ -1,7 +1,34 @@
+// Path: ./config/middlewares.js
+
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https://api.cloudinary.com'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
