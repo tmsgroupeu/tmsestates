b4fc4f7b8dfde8ab2832d1776cd79441b40ee525
tmsestates <chrysostomoss@tmsgroupeu.com>
2025-11-19 12:30:55 +0200

    test

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -17,7 +17,7 @@ export default function Footer() {
         {/* Inner container centers the content */}
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12">
             
-            <div className="flex flex-col md:flex-row justify-between items-center gap-8 border-b border-white/10 pb-8">
+            <div className="bg-background">
                 <Link href="/" className="shrink-0">
                     <Image src="/tms-logo.svg" alt="TMS Estates Logo" width={180} height={45} />
                 </Link>
