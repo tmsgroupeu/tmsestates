107817d227601865be649d5901121b182f8ab271
tmsgroupeu <chrysostomoss@tmsgroupeu.com>
2026-09-17 13:10:52 +0300

    Add GTM with Consent Mode v2

--- web/src/app/[locale]/layout.tsx
+++ web/src/app/[locale]/layout.tsx
@@ -1,6 +1,5 @@
 /* UPDATED: src/app/[locale]/layout.tsx */
 import type { Metadata } from "next";
-import Script from "next/script";
 import { Inter, Montserrat } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
@@ -29,6 +28,46 @@ type Props = {
   params: Promise<{ locale: string }>;
 };
 
+const consentModeScript = `
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+
+  gtag("consent", "default", {
+    ad_storage: "denied",
+    ad_user_data: "denied",
+    ad_personalization: "denied",
+    analytics_storage: "denied",
+    functionality_storage: "denied",
+    personalization_storage: "denied",
+    security_storage: "granted",
+    wait_for_update: 2000
+  });
+
+  gtag("set", "ads_data_redaction", true);
+  gtag("set", "url_passthrough", true);
+`;
+
+const gtmScript = `
+  (function(w,d,s,l,i){
+    w[l]=w[l]||[];
+    w[l].push({
+      'gtm.start': new Date().getTime(),
+      event:'gtm.js'
+    });
+
+    var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),
+        dl=l!='dataLayer'?'&l='+l:'';
+
+    j.async=true;
+    j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+    f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-58J57KXJ');
+`;
+
 export default async function LocaleLayout({ children, params }: Props) {
   const { locale } = await params;
 
@@ -45,12 +84,38 @@ export default async function LocaleLayout({ children, params }: Props) {
       dir={dir}
       className={`${inter.variable} ${montserrat.variable} scroll-smooth`}
     >
-      <body>
-        <Script
-          id="cookieyes"
-          src="https://cdn-cookieyes.com/client_data/152e376bf1e0ea7f9827516e01983890/script.js"
-          strategy="beforeInteractive"
+      <head>
+        {/* 1. Google Consent Mode defaults */}
+        <script
+          id="google-consent-defaults"
+          dangerouslySetInnerHTML={{ __html: consentModeScript }}
         />
+
+        {/* 2. CookieYes CMP */}
+        <script
+          id="cookieyes"
+          type="text/javascript"
+          src="https://cdn-cookieyes.com/client_data/152e376bf1e0ea7f9827516e01983890/script.js"
+        />
+
+        {/* 3. Google Tag Manager */}
+        <script
+          id="google-tag-manager"
+          dangerouslySetInnerHTML={{ __html: gtmScript }}
+        />
+      </head>
+
+      <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-58J57KXJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
 
         <NextIntlClientProvider messages={messages}>
           <Header locale={locale} />
